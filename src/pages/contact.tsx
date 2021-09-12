a76a590bcb4ec6b507ed39350c7aaaf8fdab1d8e
dufellipeR <eduardo.fellipe30@gmail.com>
2021-09-12 17:33:40 -0300

    add email sending contact

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -4,8 +4,60 @@ import Head from 'next/head';
 import Input from '../components/Input';
 import ButtonLink from '../components/ButtonLink';
 import HeaderContent from '../components/HeaderContent';
+import { useState } from 'react';
+
 
 export default function Contact() {
+
+  const [name, setName] = useState('')
+  const [company, setCompany] = useState('')
+  const [number, setNumber] = useState('')
+  const [email, setEmail] = useState('')
+  const [city, setCity] = useState('')
+  const [state, setState] = useState('')
+  const [title, setTitle] = useState('')
+  const [message, setMessage] = useState('')
+  const [submitted, setSubmitted] = useState(false)
+
+  const handleSubmit = () => { 
+    
+    console.log('Sending')
+    let data = {
+      name,
+      email,
+      company,
+      number,
+      city,
+      state, 
+      title,
+      message
+    }
+
+    fetch('/api/contact', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    }).then((res) => {
+      console.log('Response received')
+      if (res.status === 200) {
+        console.log('Response succeeded!')
+        setSubmitted(true)
+        setName('')
+        setEmail('')
+        setCompany('')
+        setNumber('')
+        setCity('')
+        setState('')
+        setTitle('')
+        setMessage('')
+      }
+    })}
+
+
+
   return (
     <>
       <Head>
@@ -37,15 +89,16 @@ export default function Contact() {
           >
             <Box w={{ sm: '100%', lg: '50%' }} p={{ sm: '2rem', lg: '0' }}>
               <Stack spacing="4">
-                <Input name="name" type="text" placeholder="Nome*" />
-                <Input name="company" type="text" placeholder="Empresa" />
-                <Input name="tel" type="text" placeholder="Telefone*" />
-                <Input name="email" type="email" placeholder="Email*" />
-                <Input name="city" type="text" placeholder="Município" />
-                <Input name="uf" type="text" placeholder="Estado" />
-                <Input name="subject" type="text" placeholder="Assunto*" />
-                <Textarea name="message" placeholder="Mensagem" focusBorderColor="red.500" bgColor="gray.200"/>
-                <ButtonLink text="Enviar" />
+                <Input onChange={(e) =>  { setName(e.target.value)}} name="name" type="text" placeholder="Nome*" />
+                <Input onChange={(e) =>  { setCompany(e.target.value)}} name="company" type="text" placeholder="Empresa" />
+                <Input onChange={(e) =>  { setNumber(e.target.value)}} name="tel" type="text" placeholder="Telefone*" />
+                <Input onChange={(e) =>  { setEmail(e.target.value)}} name="email" type="email" placeholder="Email*" />
+                <Input onChange={(e) =>  { setCity(e.target.value)}} name="city" type="text" placeholder="Município" />
+                <Input onChange={(e) =>  { setState(e.target.value)}} name="uf" type="text" placeholder="Estado" />
+                <Input onChange={(e) =>  { setTitle(e.target.value)}} name="subject" type="text" placeholder="Assunto*" />
+                <Textarea onChange={(e) => { setMessage(e.target.value)}} name="message" placeholder="Mensagem" focusBorderColor="red.500" bgColor="gray.200"/>
+                
+                <button type="button" onClick={ () => handleSubmit()}> Enviar </button>
               </Stack>
             </Box>
             <Box>
