09c0d607698e35318b165bfe174d47240f65c63f
Arthur Gramm <arthurgramm@outlook.com.br>
2021-09-12 13:57:05 -0300

    finished products line and show unique product

---
+++ src/pages/products/[slug].tsx
@@ -0,0 +1,83 @@
+import { Box, Text, Link } from '@chakra-ui/react';
+
+import { GetServerSideProps } from "next"
+import Head from "next/head";
+import { RichText } from "prismic-dom";
+
+import { getPrismicClient } from "../../services/prismic";
+
+import styles from './post.module.css';
+
+interface PostProps {
+  post: {
+    slug: string;
+    title: string;
+    content: string;
+    updatedAt: string;
+  }
+}
+
+export default function Post({ post }: PostProps) {
+  return (
+    <>
+      <Head>
+        <title>{post.title} | Ignews</title>
+      </Head>
+
+      <Box 
+        maxW={1120}
+        m="5rem auto"
+        p="0 2rem"
+      >
+        <Box 
+          maxW={720}
+          m="0 auto"
+        >
+          <Text fontSize="3.5rem" fontWeight="bold">{post.title}</Text>
+          <Text
+            display="block"
+            fontSize="1rem"
+            color="gray.400"
+            mt="1.5rem"
+          >
+            {post.updatedAt}
+          </Text>
+          <Box
+            className={styles.content}
+            mt="2rem"
+            lineHeight="2rem"
+            fontSize="1.125rem"
+            color="gray.600"
+            dangerouslySetInnerHTML={{ __html: post.content }}
+          />
+        </Box>
+      </Box>
+    </>
+  )
+
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ req, params }) => {
+  const { slug } = params;
+
+  const prismic = getPrismicClient(req);
+
+  const response = await prismic.getByUID('linha_produtos', String(slug), {});
+
+  const post = {
+    slug,
+    title: RichText.asText(response.data.title),
+    content: RichText.asHtml(response.data.content),
+    updatedAt: new Date(response.last_publication_date).toLocaleDateString('pt-BR', {
+      day: '2-digit',
+      month: 'long',
+      year: 'numeric',
+    })
+  }
+
+  return {
+    props: {
+      post,
+    }
+  }
+}
---
+++ src/pages/products/index.tsx
@@ -0,0 +1,115 @@
+import { Box, Text, Link } from '@chakra-ui/react';
+import { GetStaticProps } from 'next';
+import Head from 'next/head';
+
+import { getPrismicClient } from '../../services/prismic';
+import Prismic from '@prismicio/client';
+import { RichText } from 'prismic-dom';
+
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+}
+
+interface PostsProps {
+  posts: Post[];
+}
+
+export default function Products({ posts }: PostsProps) {
+  console.log('posts: ', posts);
+  
+  return (
+    <>
+      <Head>
+        <title>Inatruck | Produtos</title>
+      </Head>
+      
+      <Box
+        bg="#010010"
+        w="100%"
+      >
+        <Box
+          maxW={1120}
+          m="0 auto"
+          p="0 2rem"
+        >
+          <Box
+            maxW={720}
+            m="0 auto"
+          >
+            {posts.map(post => (
+              <Link
+                key={post.slug}
+                display="block"
+                borderBottomWidth="1px"
+                borderBottomStyle="solid"
+                borderBottomColor="gray.700"
+                p="2rem 0"
+                role="group"
+                href={`/products/${post.slug}`}
+              >
+                <Text
+                  fontSize="1rem"
+                  display="flex"
+                  align="center"
+                  color="gray.300"
+                >
+                  {post.updatedAt}
+                </Text>
+                <Text
+                  fontSize="1.5rem"
+                  display="block"
+                  mt="1rem"
+                  lineHeight="2rem"
+                  color="gray.100"
+                  _groupHover={{ color: '#D02129' }}
+                >
+                  {post.title}
+                </Text>
+                <Text
+                  color="gray.300"
+                  mt="0.5rem"
+                  lineHeight="1.625rem"
+                >
+                  {post.excerpt}
+                </Text>
+              </Link>
+            ))}
+          </Box>
+        </Box>
+      </Box>
+    </>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const response = await prismic.query([
+    Prismic.predicates.at('document.type', 'linha_produtos')
+  ], {
+    fetch: ['publication.title', 'publication.content'],
+    pageSize: 100,
+  });
+
+  const posts = response.results.map(post => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric',
+      })
+    }
+  })
+
+  return {
+    props: {
+      posts
+    }
+  }
+}
